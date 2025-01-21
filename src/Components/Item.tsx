b72d7c06ad20f8b292458cc25f8d158c1e9a1060
ghayth.sabeaallil <ghayth@luday.se>
2025-01-21 13:42:40 +0100

    add share

--- src/Components/Item.tsx
+++ src/Components/Item.tsx
@@ -6,7 +6,7 @@ import { SyncLoader } from "react-spinners";
 import { Carousel } from "./Carousel"
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 import { formatPrice } from "../Lib/formatPrice";
-import { IoIosShareAlt } from "react-icons/io";
+import { FacebookIcon, FacebookShareButton, TelegramIcon, TelegramShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
 
 const Item = () => {
     const [item, setItem] = useState<CardDetailsProps>();
@@ -23,22 +23,6 @@ const Item = () => {
 
     const apiKey = import.meta.env.VITE_API_KEY;
 
-    const handleShare = async () => {
-        if (navigator.share) {
-            try {
-                await navigator.share({
-                    title: document.title, // The title of the current page
-                    text: "Check out this page!",
-                    url: window.location.href, // The URL of the current page
-                });
-                console.log("Content shared successfully");
-            } catch (error) {
-                console.error("Error sharing content:", error);
-            }
-        } else {
-            alert("Web Share API is not supported in your browser.");
-        }
-    };
 
     const images = ["https://scontent-cph2-1.xx.fbcdn.net/v/t39.30808-6/473335188_583727624547034_1253354622655972216_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=wSCigR_TbRYQ7kNvgHhbGG6&_nc_zt=23&_nc_ht=scontent-cph2-1.xx&_nc_gid=AKxpcDoinxB56lqGfqUOLnN&oh=00_AYAWJ7rjU6UBpb2sUrQyJSJY4x4qvj3SR9HMkJ48ujVxqg&oe=6791D6D9", "https://scontent-cph2-1.xx.fbcdn.net/v/t39.30808-6/473547993_583726741213789_7890978986408338988_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=MjSZXHuS8AAQ7kNvgHHrRPQ&_nc_zt=23&_nc_ht=scontent-cph2-1.xx&_nc_gid=AgzjtmapyORDg2LFA-Ky7Sw&oh=00_AYDjteozoNC4VOmWe953-dZWWNUUdBDJB0KZz-2x3dqT0w&oe=6791EF23", "https://scontent-cph2-1.xx.fbcdn.net/v/t39.30808-6/473441332_583726834547113_7342916829017956123_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=lcK6MVcpbjkQ7kNvgGwNXbO&_nc_zt=23&_nc_ht=scontent-cph2-1.xx&_nc_gid=A1CU2HfHrCJIhgNTAnXsS2f&oh=00_AYASXB09usGXElkhe78Nu5LtqI5APsdlhfG7Xm8uTNhADA&oe=6791F1B0"];
     return (
@@ -74,7 +58,17 @@ const Item = () => {
 
                                 <div className="grid-cols-1 sm:grid-cols-1 md:grid-cols-2 grid justify-center gap-4">
                                     <a aria-label="تواصل عبر الواتساب" href={`https://wa.me/963${item.phone}`}><div className="border-2 border-text bg-[#25D366] text-white flex justify-center items-center gap-4 font-Amiri text-3xl p-2 rounded-xl"><p>تواصل عبر الواتساب</p><svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.014 8.00613C6.12827 7.1024 7.30277 5.87414 8.23488 6.01043L8.23339 6.00894C9.14051 6.18132 9.85859 7.74261 10.2635 8.44465C10.5504 8.95402 10.3641 9.4701 10.0965 9.68787C9.7355 9.97883 9.17099 10.3803 9.28943 10.7834C9.5 11.5 12 14 13.2296 14.7107C13.695 14.9797 14.0325 14.2702 14.3207 13.9067C14.5301 13.6271 15.0466 13.46 15.5548 13.736C16.3138 14.178 17.0288 14.6917 17.69 15.27C18.0202 15.546 18.0977 15.9539 17.8689 16.385C17.4659 17.1443 16.3003 18.1456 15.4542 17.9421C13.9764 17.5868 8 15.27 6.08033 8.55801C5.97237 8.24048 5.99955 8.12044 6.014 8.00613Z" fill="#fff"></path> <path fillRule="evenodd" clipRule="evenodd" d="M12 23C10.7764 23 10.0994 22.8687 9 22.5L6.89443 23.5528C5.56462 24.2177 4 23.2507 4 21.7639V19.5C1.84655 17.492 1 15.1767 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23ZM6 18.6303L5.36395 18.0372C3.69087 16.4772 3 14.7331 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C11.0143 21 10.552 20.911 9.63595 20.6038L8.84847 20.3397L6 21.7639V18.6303Z" fill="#fff"></path> </g></svg></div></a>
-                                    <div onClick={handleShare} className="border-2 border-text bg-white text-black flex justify-center items-center gap-4 font-Amiri text-3xl p-2 rounded-xl cursor-pointer select-none">مشاركة الرابط <IoIosShareAlt color="black" /></div>
+                                    <div className="flex justify-center items-center gap-4">
+                                        <FacebookShareButton url={window.location.href}>
+                                            <FacebookIcon size={50} round />
+                                        </FacebookShareButton>
+                                        <WhatsappShareButton url={`https://ghayth-sabeaallil.github.io/tabo/#/item?id=${item.id}`}>
+                                            <WhatsappIcon size={50} round />
+                                        </WhatsappShareButton>
+                                        <TelegramShareButton url={`https://ghayth-sabeaallil.github.io/tabo/#/item?id=${item.id}`}>
+                                            <TelegramIcon size={50} round />
+                                        </TelegramShareButton>
+                                    </div>
                                 </div>
                             </div>
                         </div>
