c888692d22605587f90356be717327d3fc3deb8a
ghayth.sabeaallil <ghayth@luday.se>
2025-01-26 01:14:13 +0100

    new design

--- src/Lib/getActive.ts
+++ src/Lib/getActive.ts
@@ -10,7 +10,7 @@ export async function getAllActive(): Promise<number> {
     return filteredData.length;
 }
 export async function getAllUnActive(): Promise<number> {
-    const response = await fetch('/DummyData.json'); // Relative path to the public folder
+    const response = await fetch('/Data.json'); // Relative path to the public folder
     const data = await response.json();
 
     const filteredData: CardDetailsProps[] = (data as CardDetailsProps[]).filter(item =>
