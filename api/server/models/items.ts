76c5819d67fb5fca68c1d6673b12b55a77e2899d
ghayth.sabeaallil <ghayth@luday.se>
2025-02-02 00:39:31 +0100

    int to double

--- api/server/models/items.ts
+++ api/server/models/items.ts
@@ -1,28 +1,32 @@
 import Mongoose from "mongoose";
 
-interface Item {
-    description: string,
-    address: string,
-    price: number,
-    rooms: number,
-    phone: number,
-    location: Location,
-    city: string,
-    area: number,
-    type: string,
-    date_created: string,
-    is_active: boolean,
-    creator: string,
-    images: string[]
-
-}
-
 interface Location {
     lat: number;
     lng: number;
 }
 
-const schema = new Mongoose.Schema<Item>({
+interface Item {
+    description: string;
+    address: string;
+    price: number;
+    rooms: number;
+    phone: number;
+    location: Location;
+    city: string;
+    area: number;
+    type: string;
+    date_created: string;
+    is_active: boolean;
+    creator: string;
+    images: string[];
+}
+
+const LocationSchema = new Mongoose.Schema<Location>({
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+});
+
+const ItemSchema = new Mongoose.Schema<Item>({
     description: { type: String, required: true },
     address: { type: String, required: true },
     city: { type: String, required: true },
@@ -31,13 +35,12 @@ const schema = new Mongoose.Schema<Item>({
     phone: { type: Number, required: true },
     rooms: { type: Number, required: true },
     area: { type: Number, required: true },
-    location: { type: Object, required: true },
+    location: { type: LocationSchema, required: true }, // Use the Location schema
     date_created: { type: String, required: true },
     is_active: { type: Boolean, required: true },
     creator: { type: String, required: true },
     images: { type: [String], required: true },
 });
-
-const ItemModel = Mongoose.model("item", schema);
+const ItemModel = Mongoose.model("item", ItemSchema);
 
 export default ItemModel;
