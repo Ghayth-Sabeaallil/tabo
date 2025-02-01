188a3a366458279a5712858d0fb34f2334cb1616
ghayth.sabeaallil <ghayth@luday.se>
2025-02-02 00:44:18 +0100

    double

--- api/server/models/items.ts
+++ api/server/models/items.ts
@@ -1,32 +1,28 @@
-import Mongoose from "mongoose";
-
-interface Location {
-    lat: number;
-    lng: number;
-}
+import Mongoose, { Double } from "mongoose";
 
 interface Item {
-    description: string;
-    address: string;
-    price: number;
-    rooms: number;
-    phone: number;
-    location: Location;
-    city: string;
-    area: number;
-    type: string;
-    date_created: string;
-    is_active: boolean;
-    creator: string;
-    images: string[];
+    description: string,
+    address: string,
+    price: number,
+    rooms: number,
+    phone: number,
+    location: Location,
+    city: string,
+    area: number,
+    type: string,
+    date_created: string,
+    is_active: boolean,
+    creator: string,
+    images: string[]
+
 }
 
-const LocationSchema = new Mongoose.Schema<Location>({
-    lat: { type: Number, required: true },
-    lng: { type: Number, required: true },
-});
+interface Location {
+    lat: Double;
+    lng: Double;
+}
 
-const ItemSchema = new Mongoose.Schema<Item>({
+const schema = new Mongoose.Schema<Item>({
     description: { type: String, required: true },
     address: { type: String, required: true },
     city: { type: String, required: true },
@@ -35,12 +31,13 @@ const ItemSchema = new Mongoose.Schema<Item>({
     phone: { type: Number, required: true },
     rooms: { type: Number, required: true },
     area: { type: Number, required: true },
-    location: { type: LocationSchema, required: true }, // Use the Location schema
+    location: { type: Object, required: true },
     date_created: { type: String, required: true },
     is_active: { type: Boolean, required: true },
     creator: { type: String, required: true },
     images: { type: [String], required: true },
 });
-const ItemModel = Mongoose.model("item", ItemSchema);
+
+const ItemModel = Mongoose.model("item", schema);
 
 export default ItemModel;
