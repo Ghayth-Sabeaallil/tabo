ceee99702b511ca1e6e947d16ed724e7068bd158
ghayth.sabeaallil <ghayth@luday.se>
2025-02-02 21:35:21 +0100

    DONE

--- client/src/Lib/getByFilter.ts
+++ client/src/Lib/getByFilter.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { CardDetailsProps } from "./DataType"
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getByFilter(
     city: string | null,
@@ -7,8 +9,19 @@ export async function getByFilter(
     price: number | null,
     area: number | null
 ): Promise<CardDetailsProps[]> {
-    const response = await fetch('/Data.json'); // Relative path to the public folder
-    const data = await response.json();
+
+    const typeSelect: Record<string, string> = {
+        "شقة": "apartments",
+        "أرض": "farms",
+        "محل": "shops",
+        "فيلا": "villas"
+    }
+    const select = typeSelect[type!];
+
+    const response = await axios.get(`${API_URL}/api/items/${select}`);
+    const data = await response.data;
+    /*const response = await fetch('/Data.json'); // Relative path to the public folder
+    const data = await response.json();*/
 
 
     const item: CardDetailsProps[] = (data as CardDetailsProps[]).filter((item) => {
