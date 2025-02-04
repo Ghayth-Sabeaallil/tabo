f33dbe957a0d160949dbff5e8efac4dd8f1160bf
ghayth.sabeaallil <ghayth@luday.se>
2025-02-04 18:42:02 +0100

    test

--- client/src/utils/getLanLngFromUrl.ts
+++ client/src/utils/getLanLngFromUrl.ts
@@ -2,6 +2,10 @@ export function getLatLngFromUrl(url: string): { lat: number, lng: number } | nu
     const regex = /@?(-?\d+\.\d+),(-?\d+\.\d+)/;
     const match = url.match(regex);
     console.log(match)
+    if (url.length < 33) {
+        const [lat, lng] = url.split(", ").map(num => parseFloat(parseFloat(num).toFixed(4)));
+        return { lat, lng };
+    }
     if (match) {
         return {
             lat: parseFloat(match[1]),
