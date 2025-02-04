7e341cf48446de851d25fccd535c49568c128d5a
ghayth.sabeaallil <ghayth@luday.se>
2025-02-04 18:44:40 +0100

    test

--- client/src/utils/getLanLngFromUrl.ts
+++ client/src/utils/getLanLngFromUrl.ts
@@ -1,11 +1,10 @@
 export function getLatLngFromUrl(url: string): { lat: number, lng: number } | null {
-    const regex = /@?(-?\d+\.\d+),(-?\d+\.\d+)/;
-    const match = url.match(regex);
-    console.log(match)
-    if (url.length < 33) {
+    if (url.length < 40) {
         const [lat, lng] = url.split(", ").map(num => parseFloat(parseFloat(num).toFixed(4)));
         return { lat, lng };
     }
+    const regex = /@?(-?\d+\.\d+),(-?\d+\.\d+)/;
+    const match = url.match(regex);
     if (match) {
         return {
             lat: parseFloat(match[1]),
