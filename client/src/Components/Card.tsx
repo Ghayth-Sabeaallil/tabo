fa744fd41579aafe868c298d45110c91806acac4
ghayth.sabeaallil <ghayth@luday.se>
2025-02-09 00:43:01 +0100

    fix card img

--- client/src/Components/Card.tsx
+++ client/src/Components/Card.tsx
@@ -10,7 +10,7 @@ const Card = ({ city, images, price, area, path }: CardDetailsProps) => {
     return (
         <>
             <div className="relative w-full h-[400px] border-2 border-text grid grid-rows-2 p-1 rounded-lg bg-card hover:bg-hoverBg hover:scale-95">
-                {images && <img loading="lazy" className="object-cover w-full h-[200px] object-cover rounded-lg border-4 border-text" src={images![0]} alt="primaryImage" />}
+                {images && <img loading="lazy" className="object-cover w-full h-[200px] object-cover rounded-lg border-4 border-text" src={images.length > 0 ? images![0] : "https://res.cloudinary.com/dornqibbw/image/upload/v1739056445/logo_q5d8tl.png"} alt="primaryImage" />}
                 <div className="relative font-Amiri text-base sm:text-base md:text-xl lg:text-xl flex flex-col items-start justify-around text-text pr-2">
                     <p>{city}</p>
                     <p>{area} {path == "farms" ? " دونم" : " متر مربع"}</p>
