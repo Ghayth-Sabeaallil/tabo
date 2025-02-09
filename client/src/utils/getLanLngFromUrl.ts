b89f8e320a597ba857cec8258dfb0be1cd55d971
ghayth.sabeaallil <ghayth@luday.se>
2025-02-09 11:55:17 +0100

    upgrade the func

--- client/src/utils/getLanLngFromUrl.ts
+++ client/src/utils/getLanLngFromUrl.ts
@@ -1,8 +1,20 @@
 export function getLatLngFromUrl(url: string): { lat: number, lng: number } | null {
     if (url.length < 40) {
-        let formattedStr = url.replace(/,/g, ", ");
-        const [lat, lng] = formattedStr.split(", ").map(num => parseFloat(parseFloat(num).toFixed(6)));
-        return { lat, lng };
+        let num = (url.match(/,/g) || []).length;
+        if (num === 1) {
+            let formattedStr = url.replace(/,/g, ', ');
+            const [lat, lng] = formattedStr
+                .split(', ')
+                .map(num => parseFloat(parseFloat(num).toFixed(6)));
+            return { lat, lng };
+        } else {
+            let newUrl = url.replace(/^([^,]*),/, '$1.').replace(/,([^,]*)$/, '.$1');
+            let formattedStr = newUrl.replace(/,/g, ', ');
+            const [lat, lng] = formattedStr
+                .split(', ')
+                .map(num => parseFloat(parseFloat(num).toFixed(6)));
+            return { lat, lng };
+        }
     } else {
         const regex = /@?(-?\d+\.\d+),(-?\d+\.\d+)/;
         const match = url.match(regex);
